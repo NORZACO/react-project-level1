a78e845702148de081dc60ab001331a80fbc4476
NORZACO <caspermotive4@gmail.com>
2023-08-04 06:53:04 +0200

    destructions and useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import { USERS } from './userGenerater'
+
+const [first_person] = USERS
+
+console.log(first_person);
 
 function App() {
+
+  // eslint-disable-next-line no-unused-vars
+  const [users, SetUser] = useState('USERS')
+  const [ emotion, SetEmotion ] = useState("EXICTING ")
+
+  // useEffect
+  useEffect(() => { console.log(`It was a very ${emotion} Film`)})
+
   return (
+    /* map first_person */
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <h1> {emotion } </h1>
+        {USERS.map((user) => (
+          <div key={user.userId}>
+            <p>Username: {user.username}</p>
+            <p>Email: {user.email}</p>
+            <p>Image: {user.avatar}</p>
+             <img src={ user.avatar } alt='welcome' height="100"/>
+             <button onClick={()=>{ console.log("clicked"); alert(`Hello ${user.avatar}`);}}> POPUP </button>
+             <button onClick={()=>{ console.log("clicked"); SetEmotion(`Hello ${user.username}`)}}> CHANGE HEAD </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/userGenerater.js
@@ -0,0 +1,23 @@
+// userGenerator.js
+
+// ESM
+// import { faker } from '@faker-js/faker';
+
+// CJS
+const { faker } = require('@faker-js/faker');
+
+export function createRandomUser() {
+  return {
+    userId: faker.datatype.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.past(30, '2003-01-01'),
+    registeredAt: faker.date.past(),
+  };
+}
+
+export const USERS = faker.helpers.multiple(createRandomUser, {
+  count: 5,
+});
