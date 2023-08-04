d6dd2b2424ebf59cbe7dadd4d7b39cbdfbce3e47
NORZACO <caspermotive4@gmail.com>
2023-08-04 22:45:45 +0200

    Creating controlled form elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import './App.css';
-import './Greeting.css';
+import "./App.css";
+import "./Greeting.css";
 import { useState, useReducer, useEffect, useRef } from "react";
-import { USERS } from './userGenerater';
+import { USERS } from "./userGenerater";
 
 const [first_person] = USERS;
 console.log(first_person);
 
 function App() {
-
-
   // eslint-disable-next-line no-unused-vars
   const [users, SetUser] = useState(USERS); // You can set the initial state to the USERS array directly
   const [emotion, SetEmotion] = useState("likes"); // Corrected the spelling of "EXCITING"
   const [secondary, setSecondary] = useState("Rainy");
   const [changecolor, changeColor] = useState("defaultColor");
 
-
   // useEffect to log the emotion whenever it changes
   useEffect(() => {
     console.log(`She is feeling ${emotion}`);
   }, [emotion]);
-
 
   // useEffect to log the secondary whenever it changes
   useEffect(() => {
@@ -29,26 +25,23 @@ function App() {
 
   // useEffect to log the changecolor whenever it changes
   useEffect(() => {
-    console.log(`The backgound color ${changecolor} have been succesfully chnaged`);
+    console.log(
+      `The backgound color ${changecolor} have been succesfully chnaged`
+    );
   }, [changecolor, emotion]);
 
+  const [checked, setChecked] = useState(false);
+  const [marked, setMarked] = useReducer((marked) => !marked, false);
+  const [played, setPlayed] = useState("#0000");
 
-  const [checked, setChecked] = useState(false)
-  const [marked, setMarked] = useReducer(marked => !marked, false)
-
-
+  console.log(played, setPlayed);
 
   // form
-
   const txtTitle = useRef();
   const hexColor = useRef();
 
-  // log txtTitle
-
-    console.log(txtTitle);
-    console.log(hexColor);
-
-
+  console.log(txtTitle);
+  console.log(hexColor);
 
   const submit = (event) => {
     event.preventDefault();
@@ -61,29 +54,55 @@ function App() {
 
     hexColor.current.value = "";
     txtTitle.current.value = "";
-    }
+  };
 
-
+  const [movei, setMovie] = useState("");
+  const [movieList, setMovieList] = useState("#00000");
+  // new form
+  const submitForm = (event) => {
+    event.preventDefault();
+    // console.log(event.target.color.value);
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    console.log(title, color);
+    // alert tltle color
+    alert(`${title} ${color}`);
+    hexColor.current.value = "";
+    txtTitle.current.value = "";
+  };
 
   return (
     /* map first_person */
     <div className="MainDiv">
-      <div className={changecolor} style={{
-        color: "red",
-        textAlign: "center",
-        padding: "10px",
-        margin: "10px",
-        borderRadius: "10px",
-        width: "fit-content",
-        height: "fit-content",
-
-
-      }}>
+      <div
+        className={changecolor}
+        style={{
+          color: "red",
+          textAlign: "center",
+          padding: "10px",
+          margin: "10px",
+          borderRadius: "10px",
+          width: "fit-content",
+          height: "fit-content",
+        }}
+      >
         <h1 style={{ textAlign: "center" }}>{emotion}</h1>
         {users.map((user) => (
+          <div
+            key={user.userId}
+            className="userId"
+            style={{
+              color: "white",
+              textAlign: "center",
+              padding: "100px",
+              margin: "10px",
+              borderRadius: "10px",
+              width: "fit-content",
+              height: "fit-content",
 
-
-          <div key={user.userId} className="userId">
+              // flexDirection: "column",
+            }}
+          >
             <p>Username: {user.username}</p>
             <p>Email: {user.email}</p>
             <p>Password: {user.password}</p>
@@ -92,44 +111,92 @@ function App() {
             <p>Feeling: {emotion}</p>
             <p>Weather: {secondary}</p>
 
-            <div className='buttonBox' style={{
-              backgroundColor: "black",
-              color: "white",
-              textAlign: "center",
-              padding: "100px",
-              margin: "10px",
-              borderRadius: "10px",
-              width: "fit-content",
-              height: "fit-content",
-              display: "flex",
-              // flexDirection: "column",
-            }}>
-              <img src={user.avatar} alt='welcome' height="100" />
-              <button onClick={() => { console.log("clicked"); alert(`Hello ${user.avatar}`); }}>POPUP</button>
-              <button onClick={() => { console.log("clicked"); SetEmotion(`Hello ${user.username}`); }}> Change name </button>
-              <button onClick={() => { console.log("clicked2"); setSecondary(`Hello Awesome`); }}> Feeling Awesome </button>
-              <button onClick={() => { console.log("clicked2"); changeColor(`changeColor`); }}> changeColor </button>
+            <div
+              className="buttonBox"
+              style={{
+                backgroundColor: "black",
+                color: "white",
+                textAlign: "center",
+                padding: "100px",
+                margin: "10px",
+                borderRadius: "10px",
+                width: "fit-content",
+                height: "fit-content",
+                display: "flex",
+                // flexDirection: "column",
+              }}
+            >
+              <img src={user.avatar} alt="welcome" height="100" />
+
+              <button
+                onClick={() => {
+                  console.log("clicked");
+                  alert(`Hello ${user.avatar}`);
+                }}
+              >
+                POPUP
+              </button>
+
+              <button
+                onClick={() => {
+                  console.log("clicked");
+                  SetEmotion(`Hello ${user.username}`);
+                }}
+              >
+                {" "}
+                Change name{" "}
+              </button>
+              <button
+                onClick={() => {
+                  console.log("clicked2");
+                  setSecondary(`Hello Awesome`);
+                }}
+              >
+                {" "}
+                Feeling Awesome{" "}
+              </button>
+              <button
+                onClick={() => {
+                  console.log("clicked2");
+                  changeColor(`changeColor`);
+                }}
+              >
+                {" "}
+                changeColor{" "}
+              </button>
             </div>
           </div>
         ))}
-        <input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)} />
+        <input
+          type="checkbox"
+          value={checked}
+          onChange={() => setChecked((checked) => !checked)}
+        />
         <p>{checked ? "checked" : "not checked"}</p>
-
         <input type="checkbox" value={marked} onChange={setMarked} />
         <p>{marked ? "marked" : "not marked"}</p> <hr></hr>
-
-
         <h1>Form</h1>
         <form onSubmit={submit}>
-            <input 
-            type="text" 
-            ref={ txtTitle } placeholder="color title" 
-            />
+          <input type="text" ref={txtTitle} placeholder="color title" />
 
-            <input ref={ hexColor } type="color" />
-            <button>ADD</button>
+          <input ref={hexColor} type="color" />
+          <button>ADD</button>
         </form>
-
+        {/* new form */}
+        <form onSubmit={submitForm}>
+          <input
+            value={movei}
+            type="text"
+            onChange={(event) => setMovie(event.target.value)}
+            placeholder="color title..."
+          />
+          <input
+            value={movieList}
+            type="color"
+            onChange={(event) => setMovieList(event.target.value)}
+          />
+          <button>ADD</button>
+        </form>
       </div>
     </div>
   );
